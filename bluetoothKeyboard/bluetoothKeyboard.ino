d32968d387bd6bbd5e97f94295a05f9f6345bc07
unknown <mdelissen@protonmail.com>
2023-11-26 01:16:07 +0100

    initial bluetooth keyboard

--- bluetoothKeyboard/bluetoothKeyboard.ino
+++ bluetoothKeyboard/bluetoothKeyboard.ino
@@ -1,11 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
+#include <Keyboard.h>
+
+const unsigned int TimeBetweenKeypressesInMs = 25;
+unsigned long NextKeypressTimeinMs;
+char LastReturnChar;
+
+void setup()
+{
+  delay(1500);
+  
+  String input;
+
+  Serial.begin(9600);
+  Serial1.begin(38400);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("test # ");
-  // delay 500 milliseconds
-  delay(500);
+void loop()
+{
+  if(Serial.available())
+  {
+    Serial1.write(Serial.read());
+  }
+
+  if (Serial1.available())
+  {
+    Serial.write(Serial1.read());
+  }
+    
 }
+
